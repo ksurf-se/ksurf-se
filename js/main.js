9877162a23fb08bd9a738046ef8c71ef3aa9d821
jtsoi <jt@nulltales.com>
2017-06-06 22:43:30 +0200

    camp-details page finished

--- js/main.js
+++ js/main.js
@@ -15,6 +15,11 @@ $(function(){
         $('#lessonsInput').val($(this).data('hours'))
     });
 
+    $('.btn-booking-request-camp').click(function(){
+        $('#packageInput').val($(this).data('package'))
+    });
+
+
 });
 
 formden.success = function(data, form_dom){
